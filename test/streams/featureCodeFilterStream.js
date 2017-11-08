5711ac38ccd6516c761662c95ae1556f24524bd6
Tyler Pedelose <pedelose.1@gmail.com>
2017-11-08 15:45:18 -0500

    Fixed the featureCodeFilter test to reflect new schema

--- test/streams/featureCodeFilterStream.js
+++ test/streams/featureCodeFilterStream.js
@@ -5,13 +5,13 @@ var filterRecord = featureCodeFilterStream.filterRecord;
 
 tape('feature code filters', function(test) {
   test.test('filtered out records will be removed', function(t) {
-    var record = { feature_code: 'RGNE' };
+    var record = { dsg: 'RGNE' };
     t.notOk(filterRecord(record), 'RGNE is filtered out');
     t.end();
   });
 
   test.test('good records are not removed', function(t) {
-    var record = { feature_code: 'ADM1' };
+    var record = { dsg: 'ADM1' };
     t.ok(filterRecord(record), 'ADM1 is not filtered out');
     t.end();
   });
