1548d963e5d2c63f1b397464563a5def31ff3785
Tyler Pedelose <pedelose.1@gmail.com>
2018-03-06 17:22:31 -0500

    Added completion line to download progress

--- bin/download_data.js
+++ bin/download_data.js
@@ -9,16 +9,16 @@ const logger = require( 'pelias-logger' ).get( 'geonamesmil' ),
       fs = require('fs'),
       Table = require('cli-table');
 
-const config = require('pelias-config').generate();
-const basepath = config.imports.geonamesmil.datapath;
-const isocode = validateISOCode(config.imports.geonamesmil.countryCode);
+const config = require('pelias-config').generate(),
+      basepath = config.imports.geonamesmil.datapath,
+      isocode = validateISOCode(config.imports.geonamesmil.countryCode);
 
-const filenames = require('../metadata/isocodes.json');
-const filename = isocode === 'ALL' ? filenames.all : isocode;
+const filenames = require('../metadata/isocodes.json'),
+      filename = isocode === 'ALL' ? filenames.all : isocode;
 
+const remoteFilePath = `http://geonames.nga.mil/gns/html/cntyfile/${filename}.zip`,
+      localFileName = `${basepath}/${filename}.zip`;
 
-const remoteFilePath = `http://geonames.nga.mil/gns/html/cntyfile/${filename}.zip`;
-const localFileName = `${basepath}/${filename}.zip`;
 logger.info( 'downloading datafile from:', remoteFilePath );
 
 
@@ -46,6 +46,17 @@ function secondsToHMS(seconds) {
   return util.format('%s:%s:%s', h, m, s);
 }
 
+function overwriteTTYLine(table, logArray) {
+  table.pop();
+  table.push( logArray );
+  var logLine = table.toString().split('\n')[1];
+
+  // Write the progress to console
+  process.stdout.clearLine();
+  process.stdout.cursorTo( 0 );
+  process.stdout.write( logLine );
+}
+
 
 var table = new Table({
     head: ['%     ','Received Data','Total Data','Time Left','Time Spent','Time Total'],
@@ -54,33 +65,38 @@ var table = new Table({
         'left': '', 'left-mid': '', 'mid': '', 'mid-mid': '', 'right': '',
         'right-mid': '', 'middle': '   ' },
 });
-table.push([0, 0, 0, '00:00:00', '00:00:00', '00:00:00']);
+var zeroTimeStr = '00:00:00',
+    hundredPercentStr = precisionRound(100, 2).toFixed(2),
+    total = formatBytes(0),
+    timeTotalStr = zeroTimeStr;
+
+
+table.push([0, 0, 0, zeroTimeStr, zeroTimeStr, zeroTimeStr]);
 process.stdout.write(table.toString());
 
 progress(request(remoteFilePath), {})
   .on('progress', function (state) {
     var percent = precisionRound(state.percent*100, 2).toFixed(2),
-        total = formatBytes(state.size.total),
         received = formatBytes(state.size.transferred),
         timeSpent = precisionRound(state.time.elapsed, 0),
         timeLeft = precisionRound(state.time.remaining, 0),
-        timeTotal = timeLeft + timeSpent;
+        timeTotal = timeLeft + timeSpent,
+        timeSpentStr = secondsToHMS(timeSpent),
+        timeLeftStr = secondsToHMS(timeLeft);
 
-    var logArray = [ percent, received, total, timeLeft, timeSpent, timeTotal ];
+    total = formatBytes(state.size.total);
+    timeTotalStr = secondsToHMS(timeTotal);
 
-    table.pop();
-    table.push( logArray );
-    var logLine = table.toString().split('\n')[1];
-
-    // Write the progress to console
-    process.stdout.clearLine();
-    process.stdout.cursorTo( 0 );
-    process.stdout.write( logLine );
+    var logArray = [ percent, received, total, timeLeftStr, timeSpentStr, timeTotalStr ];
+    overwriteTTYLine(table, logArray);
   })
   .on('error', function (err) {
     process.stderr.write(err);
   })
   .on('end', function () {
-    console.log( 'Done downloadin', filename );
+    var logArray = [ hundredPercentStr, total, total, zeroTimeStr, timeTotalStr, timeTotalStr ];
+    overwriteTTYLine(table, logArray);
+    console.log( '\n' );
+    logger.info( 'Done downloading', filename );
   })
   .pipe(fs.createWriteStream(localFileName));
